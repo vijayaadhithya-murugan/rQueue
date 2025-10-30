180b70fa93076b90532d38bec69aaafd2f584790
16201 <16201@ramco.com>
2025-10-30 19:28:30 +0530

    Updated vite Config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -11,6 +12,11 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       base: '/rQueue/',
+      css: {
+        postcss: {
+          plugins: [tailwindcss],
+        },
+      },
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
