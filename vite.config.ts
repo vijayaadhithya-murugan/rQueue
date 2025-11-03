4f5531af98f860af161f75267393016bed6a5458
Annlyn Sarah JP <AnnlynSarah.JP+Ramco@ramco.com>
2025-11-03 10:55:44 +0530

    updated config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from 'tailwindcss';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -12,18 +11,13 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       base: '/rQueue/',
-      css: {
-        postcss: {
-          plugins: [tailwindcss],
-        },
-      },
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '.'),
+          '@': path.resolve(__dirname, 'src'),
         }
       }
     };
